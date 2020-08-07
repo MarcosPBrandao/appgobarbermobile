47f0e1b748f76075c43be53cae1894afd0620ac5
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-08-07 16:00:14 -0300

    concluido dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,8 +2,15 @@ import React, { createContext, useCallback, useState, useContext, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    avatar_url: string;
+}
+
 interface AuthState {
-  user: object;
+  user: User;
   token: string;
 }
 
@@ -13,7 +20,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -30,7 +37,9 @@ export const AuthProvider: React.FC = ({ children }) => {
             '@goBarber:token',
             '@goBarber:user'
         ]);
+        api.defaults.headers.authorization = `Bearer ${token}`;
         if (token[1] && user[1]) {
+            api.defaults.headers.authorization = `Bearer ${token[1]}`;
             setData({token: token[1],user: JSON.parse(user[1]) })
         }
         setLoading(false);
@@ -39,7 +48,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     loadStorageData();
   }, []);
   const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post<{ token: string; user: object }>(
+    const response = await api.post(
       'sessions',
       {
         email,
@@ -51,6 +60,7 @@ export const AuthProvider: React.FC = ({ children }) => {
         ['@goBarber:token', token],
         ['@goBarber:user', JSON.stringify(user)]
     ]);
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
   const signOut = useCallback(async () => {
